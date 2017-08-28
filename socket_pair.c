83cb8f9c87ef38c36ac3421458b81ba7db024b6b
liuzexin <10848224@qq.com>
2017-08-28 19:42:36 +0800

    modified:   socket_pair.c

--- socket_pair.c
+++ socket_pair.c
@@ -8,27 +8,26 @@
 
 int main(void)
 {
-    int sv[2]; /* the pair of socket descriptors */
-    char buf; /* for data exchange between processes */
+    int sv[2]; 
+    char buf; 
     
     if (socketpair(AF_UNIX, SOCK_STREAM, 0, sv) == -1) {
         perror("socketpair");
         exit(1);
     }
-                                        
-    if (!fork()) {  /* child */
+    if (!fork()) {  
         read(sv[1], &buf, 1);
         printf("child: read '%c'\n", buf);
-        buf = toupper(buf);  /* make it uppercase */
+        buf = toupper(buf);  
         write(sv[1], &buf, 1);
         printf("child: sent '%c'\n", buf);
                                                                                         
-    } else { /* parent */
+    } else { 
         write(sv[0], "b", 1);
         printf("parent: sent 'b'\n");
         read(sv[0], &buf, 1);
         printf("parent: read '%c'\n", buf);
-        wait(NULL); /* wait for child to die */
+        wait(NULL); 
     }
     return 0;
 }
